c3c159cce0f8b74e883e2f5771ec380582b8b8c7
Vineeth Madishetti <vineethmadishetti@gmail.com>
2025-12-18 23:48:48 +0530

    frontend issues

---
+++ client/src/pages/AdminPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPanel = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <p>This is the main dashboard view. You can add file upload functionality and statistics here.</p>
+    </div>
+  );
+};
+
+export default AdminPanel;
---
+++ client/src/pages/SearchDatabase.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchDatabase = () => {
+  return (
+    <div>
+      <h1>Search Database</h1>
+      <p>The main candidate search interface will be displayed here.</p>
+    </div>
+  );
+};
+
+export default SearchDatabase;
