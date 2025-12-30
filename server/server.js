3ddc3cdb3c6212090bd83fb5c28dadf064fbd294
Vineeth Madishetti <vineethmadishetti@gmail.com>
2025-12-30 13:50:33 +0530

    linux server mongo

--- server/server.js
+++ server/server.js
@@ -83,6 +83,8 @@ app.use(
     origin: [
       'http://localhost:5173',
       'https://hirextra-frontend.onrender.com',
+      'http://stucrow.com',
+      'https://stucrow.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
