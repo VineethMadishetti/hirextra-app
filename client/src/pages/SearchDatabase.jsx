4259db3ab75011e8292ff3eb2d4be3bc8da9cc3e
Vineeth Madishetti <vineethmadishetti@gmail.com>
2026-03-31 21:25:24 +0530

    name change

--- client/src/pages/SearchDatabase.jsx
+++ client/src/pages/SearchDatabase.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchDatabase = () => {
   return (
     <div>
-      <h1>AI Search Database</h1>
+      <h1>AI Talent Search</h1>
       <p>The main candidate search interface will be displayed here.</p>
     </div>
   );
